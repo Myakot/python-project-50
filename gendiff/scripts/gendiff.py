13d448849ca68cfa82583c38f5bf1f407f484a5a
Myakot <cstepan@yandex.ru>
2023-11-23 20:00:58 +0700

    step_2, added optional argument format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,12 +10,17 @@ print(args.echo)
 
 '''
 gendiff -h
-usage: gendiff [-h] first_file second_file
+usage: gendiff [-h] [-f FORMAT] first_file second_file
+
 Compares two configuration files and shows a difference.
+
 positional arguments:
   first_file
   second_file
+
 optional arguments:
   -h, --help            show this help message and exit
+  -f FORMAT, --format FORMAT
+                        set format of output
 '''
 
