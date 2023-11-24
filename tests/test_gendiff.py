4581b0d9eb95d2a85542508a1e7b4bcc047079da
Myakot <cstepan@yandex.ru>
2023-11-24 19:45:55 +0700

    added a test for empty .json, made make test verbose

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -9,3 +9,8 @@ def test_generate_diff():
 def test_generate_diff_yaml():
     result = open('tests/fixtures/correct.json')
     assert run_gendiff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml') == print(result)
+
+
+def test_generate_diff_empty():
+    result = open('tests/fixtures/empty.json')
+    assert run_gendiff('tests/fixtures/file1.json', 'tests/fixtures/file1.json') == print(result)
