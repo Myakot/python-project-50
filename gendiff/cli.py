b363737263e7a3e57c92d1b50fa4cac19f20c0b0
Myakot <cstepan@yandex.ru>
2023-12-01 14:50:38 +0700

    moved arg parser to separate cli.py module

---
+++ gendiff/cli.py
@@ -0,0 +1,17 @@
+import argparse
+from tests.CONSTANTS import DESCRIPTION_GENDIFF
+
+
+def parse_args():
+    """ Separate CLI module that is responsible for working with
+    the console the function returns a tuple (file1, file2, formatter) """
+
+    parser = argparse.ArgumentParser(description=DESCRIPTION_GENDIFF)
+    parser.add_argument("first_file",
+                        help='Path to json or yml file')
+    parser.add_argument("second_file",
+                        help='Path to second file in same format')
+    parser.add_argument("-f", "--format", default='stylish',
+                        choices=['stylish', 'plain', 'json'],
+                        help='set format of output: stylish, plain or json')
+    return parser.parse_args()
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python3
-import argparse
 from gendiff.engine import generate_diff
-from tests.CONSTANTS import DESCRIPTION_GENDIFF
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description=DESCRIPTION_GENDIFF)
-    parser.add_argument("first_file",
-                        help='Path to json or yml file')
-    parser.add_argument("second_file",
-                        help='Path to second file in same format')
-    parser.add_argument("-f", "--format", default='stylish',
-                        choices=['stylish', 'plain', 'json'],
-                        help='set format of output: stylish, plain or json')
-    return parser.parse_args()
+from gendiff.cli import parse_args
 
 
 def main():
