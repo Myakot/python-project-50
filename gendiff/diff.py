89efad3003bec357d058806617faebeecf997f5b
Myakot <cstepan@yandex.ru>
2023-12-07 17:37:09 +0700

    diff.py was simplified and made readable

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,26 +1,27 @@
 def create_diff(data_1: dict, data_2: dict) -> list:
-    added_names, removed_names = (set(data_1) - set(data_2),
-                                  set(data_2) - set(data_1))
-    common_names = set(data_1) & set(data_2)
-    result = list()
-    names = list(added_names | removed_names | common_names)
-    for name in sorted(names):
-        new_item = {'name': name}
-        if name in added_names or name in removed_names:
-            new_item['type'] = 'removed' if name in added_names else 'added'
-            new_item['value'] = \
-                data_1[name] if name in added_names else data_2[name]
-            result.append(new_item)
-            continue
-        value_1, value_2 = data_1[name], data_2[name]
-        if isinstance(value_1, dict) and isinstance(value_2, dict):
-            new_item['type'] = 'nested'
-            new_item['children'] = create_diff(value_1, value_2)
-        elif value_1 == value_2:
-            new_item['type'] = 'unchanged'
-            new_item['value'] = value_1
+    diff = list()
+    keys = set(data_1) | set(data_2)
+
+    for key in sorted(keys):
+        new_item = {'name': key}
+
+        if key not in data_1:
+            new_item['type'] = 'added'
+            new_item['value'] = data_2[key]
+        elif key not in data_2:
+            new_item['type'] = 'removed'
+            new_item['value'] = data_1[key]
         else:
-            new_item['type'] = 'changed'
-            new_item['value'] = value_1, value_2
-        result.append(new_item)
-    return result
+            value_1, value_2 = data_1[key], data_2[key]
+            if isinstance(value_1, dict) and isinstance(value_2, dict):
+                new_item['type'] = 'nested'
+                new_item['children'] = create_diff(value_1, value_2)
+            elif value_1 == value_2:
+                new_item['type'] = 'unchanged'
+                new_item['value'] = value_1
+            else:
+                new_item['type'] = 'changed'
+                new_item['value'] = value_1, value_2
+        diff.append(new_item)
+
+    return diff
--- gendiff/formatters/formatter.py
+++ gendiff/formatters/formatter.py
@@ -5,11 +5,10 @@ from gendiff.formatters.json import json_format
 
 def stringify_diff(diff: list, print_format: str) -> str:
     if print_format == 'plain':
-        result = plain_format(diff)
+        return plain_format(diff)
     elif print_format == 'json':
-        result = json_format(diff)
+        return json_format(diff)
     elif print_format == 'stylish':
-        result = stylish_format(diff)
+        return stylish_format(diff)
     else:
         raise FileNotFoundError(f'Unknown format: {print_format}')
-    return result
