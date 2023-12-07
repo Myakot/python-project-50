9b40b2efe08a6a07449dd8479883a92535e4ec29
Myakot <cstepan@yandex.ru>
2023-12-07 17:56:56 +0700

    imported constant variable as non-constant - fixed it

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -8,28 +8,28 @@ from gendiff.formatters.stylish import stylish_format
 from gendiff.formatters.plain import plain_format
 from gendiff.formatters.json import json_format
 from gendiff.formatters.formatter import stringify_diff
-import tests.CONSTANTS as path
+import tests.CONSTANTS as PATH
 
 
 # В очередной раз спасибо комментариям к шагам проекта за декоратор parametrize
 @pytest.mark.parametrize('file1_path, file2_path, path_to_result',
-                         [(path.PATH_JSON_FLAT_1, path.PATH_JSON_FLAT_2, path.PATH_JSON_FLAT_RESULT),
-                          (path.PATH_YML_FLAT_1, path.PATH_YML_FLAT_2, path.PATH_YML_FLAT_RESULT),
-                          (path.PATH_JSON_1_1, path.PATH_JSON_1_2, path.PATH_JSON_1_RESULT),
-                          (path.PATH_JSON_2_1, path.PATH_JSON_2_2, path.PATH_JSON_2_RESULT)])
+                         [(PATH.PATH_JSON_FLAT_1, PATH.PATH_JSON_FLAT_2, PATH.PATH_JSON_FLAT_RESULT),
+                          (PATH.PATH_YML_FLAT_1, PATH.PATH_YML_FLAT_2, PATH.PATH_YML_FLAT_RESULT),
+                          (PATH.PATH_JSON_1_1, PATH.PATH_JSON_1_2, PATH.PATH_JSON_1_RESULT),
+                          (PATH.PATH_JSON_2_1, PATH.PATH_JSON_2_2, PATH.PATH_JSON_2_RESULT)])
 def test_generate_diff(file1_path, file2_path, path_to_result):
     with open(path_to_result) as result_file:
         assert generate_diff(file1_path, file2_path) == result_file.read()
 
 
 @pytest.mark.parametrize('file1_path, file2_path, print_format, path_to_result',
-                         [(path.PATH_YML_1, path.PATH_YML_2, 'stylish', path.PATH_YAML_RESULT),
-                          (path.PATH_YML_1, path.PATH_YML_2, 'plain', path.PLAIN_1_RESULT),
-                          (path.PATH_YML_1, path.PATH_YML_2, 'json', path.JSON_FORMAT_RESULT_1),
-                          (path.PATH_JSON_1_1, path.PATH_JSON_1_2, 'plain', path.PLAIN_1_RESULT),
-                          (path.PATH_JSON_2_1, path.PATH_JSON_2_2, 'plain', path.PLAIN_2_RESULT),
-                          (path.PATH_JSON_1_1, path.PATH_JSON_1_2, 'json', path.JSON_FORMAT_RESULT_1),
-                          (path.PATH_JSON_2_1, path.PATH_JSON_2_2, 'json', path.JSON_FORMAT_RESULT_2)])
+                         [(PATH.PATH_YML_1, PATH.PATH_YML_2, 'stylish', PATH.PATH_YAML_RESULT),
+                          (PATH.PATH_YML_1, PATH.PATH_YML_2, 'plain', PATH.PLAIN_1_RESULT),
+                          (PATH.PATH_YML_1, PATH.PATH_YML_2, 'json', PATH.JSON_FORMAT_RESULT_1),
+                          (PATH.PATH_JSON_1_1, PATH.PATH_JSON_1_2, 'plain', PATH.PLAIN_1_RESULT),
+                          (PATH.PATH_JSON_2_1, PATH.PATH_JSON_2_2, 'plain', PATH.PLAIN_2_RESULT),
+                          (PATH.PATH_JSON_1_1, PATH.PATH_JSON_1_2, 'json', PATH.JSON_FORMAT_RESULT_1),
+                          (PATH.PATH_JSON_2_1, PATH.PATH_JSON_2_2, 'json', PATH.JSON_FORMAT_RESULT_2)])
 def test_generate_diff_format(file1_path, file2_path, print_format, path_to_result):
     with open(path_to_result) as result_file:
         assert generate_diff(file1_path,
@@ -39,7 +39,7 @@ def test_generate_diff_format(file1_path, file2_path, print_format, path_to_resu
 
 def test_open_exception():
     with pytest.raises(FileNotFoundError):
-        get_format_and_data(path.PATH_JSON_FLAT_RESULT)
+        get_format_and_data(PATH.PATH_JSON_FLAT_RESULT)
 
 
 def test_stringify_exception():
@@ -48,9 +48,9 @@ def test_stringify_exception():
 
 
 def test_diff():
-    data_1 = parse(*get_format_and_data(path.PATH_JSON_1_1))
+    data_1 = parse(*get_format_and_data(PATH.PATH_JSON_1_1))
     data_1_copy = copy.deepcopy(data_1)
-    data_2 = parse(*get_format_and_data(path.PATH_JSON_1_2))
+    data_2 = parse(*get_format_and_data(PATH.PATH_JSON_1_2))
     data_2_copy = copy.deepcopy(data_2)
     create_diff(data_1, data_2)
     assert data_1 == data_1_copy
@@ -58,8 +58,8 @@ def test_diff():
 
 
 def test_print():
-    data_1 = parse(*get_format_and_data(path.PATH_JSON_1_1))
-    data_2 = parse(*get_format_and_data(path.PATH_JSON_1_2))
+    data_1 = parse(*get_format_and_data(PATH.PATH_JSON_1_1))
+    data_2 = parse(*get_format_and_data(PATH.PATH_JSON_1_2))
     diff = create_diff(data_1, data_2)
     diff_copy = copy.deepcopy(diff)
     stylish_format(diff)
