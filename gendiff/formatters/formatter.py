3aea28448b5b1fbd772370fbded04f19f68822eb
Myakot <cstepan@yandex.ru>
2023-12-12 23:59:44 +0700

    switched stringidy_diff to match-case style of new python

--- gendiff/formatters/formatter.py
+++ gendiff/formatters/formatter.py
@@ -4,11 +4,12 @@ from gendiff.formatters.json import json_format
 
 
 def stringify_diff(diff: list, print_format: str) -> str:
-    if print_format == 'plain':
-        return plain_format(diff)
-    elif print_format == 'json':
-        return json_format(diff)
-    elif print_format == 'stylish':
-        return stylish_format(diff)
-    else:
-        raise FileNotFoundError(f'Unknown format: {print_format}')
+    match print_format:
+        case 'plain':
+            return plain_format(diff)
+        case 'json':
+            return json_format(diff)
+        case 'stylish':
+            return stylish_format(diff)
+        case _:
+            raise FileNotFoundError(f'Unknown format: {print_format}')
