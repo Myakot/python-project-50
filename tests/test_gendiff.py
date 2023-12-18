368945dc33901abe7999db64b99ff62bd4273eb0
Myakot <cstepan@yandex.ru>
2023-12-18 23:19:23 +0700

    got rid of unnecessary testing

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,27 +1,15 @@
 import pytest
-import copy
 
 
 from gendiff.generate_difference import generate_diff, get_format_and_data
-from gendiff.parser import parse
-from gendiff.diff_tree import create_diff
-from gendiff.formatters.stylish import stylish_format
-from gendiff.formatters.plain import to_plain
-from gendiff.formatters.json import json_format
 from gendiff.formatters import stringify_diff
 from gendiff.utils import get_file_path
-import tests.CONSTANTS as PATH
+from tests.CONSTANTS import PATH_JSON_FLAT_RESULT
 
 
 @pytest.mark.parametrize(
     "file1_path, file2_path, path_to_result",
     [
-        pytest.param(
-            'file_1_flat.json',
-            'file_2_flat.json',
-            'file_flat_result_json',
-            id="flat_json_file"
-        ),
         pytest.param(
             'file_1_flat.yml',
             'file_2_flat.yaml',
@@ -116,30 +104,9 @@ def test_generate_diff_format(file1_path, file2_path, print_format, path_to_resu
 
 def test_open_exception():
     with pytest.raises(FileNotFoundError):
-        get_format_and_data(PATH.PATH_JSON_FLAT_RESULT)
+        get_format_and_data(PATH_JSON_FLAT_RESULT)
 
 
 def test_stringify_exception():
     with pytest.raises(ValueError):
         stringify_diff('diff_data', 'unknown format')
-
-
-def test_diff():
-    data_1 = parse(*get_format_and_data(PATH.PATH_JSON_1_1))
-    data_1_copy = copy.deepcopy(data_1)
-    data_2 = parse(*get_format_and_data(PATH.PATH_JSON_1_2))
-    data_2_copy = copy.deepcopy(data_2)
-    create_diff(data_1, data_2)
-    assert data_1 == data_1_copy
-    assert data_2 == data_2_copy
-
-
-def test_print():
-    data_1 = parse(*get_format_and_data(PATH.PATH_JSON_1_1))
-    data_2 = parse(*get_format_and_data(PATH.PATH_JSON_1_2))
-    diff = create_diff(data_1, data_2)
-    diff_copy = copy.deepcopy(diff)
-    stylish_format(diff)
-    to_plain(diff)
-    json_format(diff)
-    assert diff == diff_copy
