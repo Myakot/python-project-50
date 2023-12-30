ef2dd466f28347f078c9752fa7c3a8eb849d0ab2
Myakot <cstepan@yandex.ru>
2023-12-30 23:28:13 +0700

    changed tests for more readability

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -8,27 +8,13 @@ from tests.CONSTANTS import PATH_JSON_FLAT_RESULT
 
 
 @pytest.mark.parametrize(
-    "file1_path, file2_path, path_to_result",
+    'file1_path, file2_path, path_to_result',
     [
-        pytest.param(
-            'file_1_flat.yml',
-            'file_2_flat.yaml',
-            'file_flat_result_yaml',
-            id="flat_yaml_file"
-        ),
-        pytest.param(
-            'file_1_1.json',
-            'file_1_2.json',
-            'file_1_result_json',
-            id="json_file"
-        ),
-        pytest.param(
-            'file_2_1.json',
-            'file_2_2.json',
-            'file_2_result_json',
-            id="json_2_file"
-        ),
+        ('file_1_flat.yml', 'file_2_flat.yaml', 'file_flat_result_yaml'),
+        ('file_1_1.json', 'file_1_2.json', 'file_1_result_json'),
+        ('file_2_1.json', 'file_2_2.json', 'file_2_result_json'),
     ],
+    ids=['flat_yaml_file',  'json_file', 'json_2_file']
 )
 def test_generate_diff(file1_path, file2_path, path_to_result):
     expected_path = get_file_path(path_to_result)
@@ -40,59 +26,18 @@ def test_generate_diff(file1_path, file2_path, path_to_result):
 
 
 @pytest.mark.parametrize(
-    "file1_path, file2_path, print_format,  path_to_result",
+    'file1_path, file2_path, print_format, path_to_result',
     [
-        pytest.param(
-            'file_1_1.yml',
-            'file_1_2.yaml',
-            'stylish',
-            'file_1_result_json',
-            id="yml_yaml_stylish"
-        ),
-        pytest.param(
-            'file_1_1.yml',
-            'file_1_2.yaml',
-            'json',
-            'file_1_format.json',
-            id="yaml_to_json"
-        ),
-        pytest.param(
-            'file_1_1.yml',
-            'file_1_2.yaml',
-            'plain',
-            'file_1_result_plain',
-            id="yaml_to_plain"
-        ),
-        pytest.param(
-            'file_1_1.json',
-            'file_1_2.json',
-            'plain',
-            'file_1_result_plain',
-            id="json_plain_file"
-        ),
-        pytest.param(
-            'file_2_1.json',
-            'file_2_2.json',
-            'plain',
-            'file_2_plain_result',
-            id="json_2_plain_file"
-        ),
-        pytest.param(
-            'file_1_1.json',
-            'file_1_2.json',
-            'json',
-            'file_1_format.json',
-            id="json_format_file"
-        ),
-        pytest.param(
-            'file_2_1.json',
-            'file_2_2.json',
-            'json',
-            'file_2_format.json',
-            id="json_2_format_file"
-        ),
+        ('file_1_1.yml', 'file_1_2.yaml', 'stylish', 'file_1_result_json'),
+        ('file_1_1.yml', 'file_1_2.yaml', 'json', 'file_1_format.json'),
+        ('file_1_1.yml', 'file_1_2.yaml', 'plain', 'file_1_result_plain'),
+        ('file_1_1.json', 'file_1_2.json', 'plain', 'file_1_result_plain'),
+        ('file_2_1.json', 'file_2_2.json', 'plain', 'file_2_plain_result'),
+        ('file_1_1.json', 'file_1_2.json', 'json', 'file_1_format.json'),
+        ('file_2_1.json', 'file_2_2.json', 'json', 'file_2_format.json'),
     ],
-)
+    ids=['yml_yaml_stylish',  'yaml_to_json', 'yaml_to_plain', 'json_plain_file',
+         'json_2_plain_file', 'json_format_file', 'json_2_format_file'])
 def test_generate_diff_format(file1_path, file2_path, print_format, path_to_result):
     expected_path = get_file_path(path_to_result)
     with open(expected_path, 'r') as file:
